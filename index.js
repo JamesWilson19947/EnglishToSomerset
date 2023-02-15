c294f2c71afa9e0e2bc08e85060b76deb324f2b9
James Wilson <me@jamesphilipwilson.co.uk>
2023-02-15 22:16:42 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const somersetize = require('./somersetize');
+const somersetize = require('/somersetize');
 
 const app = express();
 
