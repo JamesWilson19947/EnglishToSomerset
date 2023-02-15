634ed127edd905eb4533b91ebca59e85f9dea24c
James Wilson <me@jamesphilipwilson.co.uk>
2023-02-15 22:02:07 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const somersetize = require('./somersetize');
+
+const app = express();
+
+// Use json middleware to parse request body
+app.use(bodyParser.json());
+
+// Define route for /somersetize endpoint
+app.post('/somersetize', (req, res) => {
+  const input = req.body.input;
+  const output = somersetize(input);
+
+  console.log(output)
+  res.send({ output: output });
+});
+
+// Serve index.html at root path
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
---
+++ somersetize.js
@@ -0,0 +1,42 @@
+const dictionary = require('./translations/dictionary.json');
+const randomSayings = require('./translations/somersetSayings.json');
+
+const somersetize = (input) => {
+  console.log(input)
+  
+  const words = input.split(' ');
+
+  const translatedWords = words.map(word => {
+    const lowerCaseWord = word.toLowerCase();
+
+    if (dictionary.hasOwnProperty(lowerCaseWord)) {
+      return dictionary[lowerCaseWord];
+    }
+
+    return word;
+  });
+
+  const outputWords = [];
+
+  for (let i = 0; i < translatedWords.length; i++) {
+    outputWords.push(translatedWords[i]);
+
+    // Add random saying with a 5% chance
+    if (i < translatedWords.length - 1 && Math.random() < 0.02) {
+      const randomSaying = randomSayings[Math.floor(Math.random() * randomSayings.length)];
+      outputWords.push(randomSaying);
+    }
+  }
+
+  const output = outputWords.join(' ')
+
+  // Add random saying at the beginning with a 5% chance
+  if (Math.random() < 0.05) {
+    const randomSaying = randomSayings[Math.floor(Math.random() * randomSayings.length)];
+    return randomSaying + ' ' + output;
+  }
+
+  return output;
+};
+
+module.exports = somersetize;
